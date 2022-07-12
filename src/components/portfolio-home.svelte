71b1e1f2e1433ab9b738eab60d69cbaa7514686a
oguerrero <oscarguerrero242@gmail.com>
2022-07-12 14:45:45 -0500

    added img and about me

--- src/components/portfolio-home.svelte
+++ src/components/portfolio-home.svelte
@@ -1,11 +1,16 @@
-<section class="mx-auto px-6 py-4">
-    <h1 class="text-3xl">
-        Hola, mi nombre es <strong>Oscar Guerrero</strong>
-        y soy un Fullstack Developer.
-    </h1>
-    <article>
-        Soy un Fullstack Developer, me encanta aprender nuevas tecnologías que faciliten el desarrollo.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi perspiciatis nostrum saepe, 
-        repellat iste magnam unde praesentium blanditiis exercitationem.
+<section class="mx-auto px-8 py-4 md:px-20 grid md:grid-cols-2">
+    <article class="sm:row-start-2 md:row-auto">
+        <h1 class="text-3xl text-center">
+            Hola, mi nombre es <strong>Oscar Guerrero</strong>
+            y soy un Fullstack Developer.
+        </h1>
+        <p class="py-2 text-lg md:px-20">
+            Me encanta aprender nuevas tecnologías que faciliten y 
+            agilicen el desarrollo de aplicaciones web.<br> Actualmente estoy
+            aprendiendo a usar <strong>Svelte</strong> para crear aplicaciones web.
+        </p>
+    </article>
+    <article class="justify-center text-center">
+        <img src="/images/logo.jpg" alt="logo" class="logo">
     </article>
 </section>
