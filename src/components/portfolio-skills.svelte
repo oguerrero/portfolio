147c9720baffa40095744edf0635fe348d43fe57
oguerrero <oscarguerrero242@gmail.com>
2022-07-12 16:24:07 -0500

    added skills section

---
+++ src/components/portfolio-skills.svelte
@@ -0,0 +1,64 @@
+<section class="p-6">
+    <article class="flex flex-wrap gap-x-10 gap-y-8 justify-around md:gap-x-2 lg:gap-x-2">
+        <div class="card shadow-orange-600">
+            <img src="images/logo-html.svg" alt="">
+            <p class="text-center pt-2">
+                html
+            </p>
+        </div>
+        <div class="card shadow-yellow-600">
+             <img src="images/logo-js.svg" alt="">
+             <p class="text-center pt-4">
+                 js
+             </p>
+        </div>
+        <div class="card shadow-blue-600">
+             <img src="images/logo-css.svg" alt="">
+             <p class="text-center pt-2">
+                 css
+             </p>
+        </div>
+        <div class="card shadow-orange-600">
+             <img src="images/logo-svelte.svg" alt="">
+             <p class="text-center pt-2">
+                 svelte
+             </p>
+        </div>
+        <div class="card shadow-blue-600">
+             <img src="images/logo-tailwind.svg" alt="">
+             <p class="text-center pt-2">
+                 tailwindcss
+             </p>
+        </div>
+        <div class="card shadow-yellow-600">
+             <img src="images/logo-python.svg" alt="">
+             <p class="text-center pt-2">
+                 python
+             </p>
+        </div>
+        <div class="card shadow-orange-600">
+             <img src="images/logo-java.svg" alt="">
+             <p class="text-center pt-2">
+                 Java
+             </p>
+        </div>
+        <div class="card shadow-red-600">
+             <img src="images/logo-git.svg" alt="">
+             <p class="text-center pt-2">
+                 Git
+             </p>
+        </div>
+        <div class="card shadow-blue-600">
+             <img src="images/logo-mysql.svg" alt="">
+             <p class="text-center pt-2">
+                 MySQL
+             </p>
+        </div>
+        <div class="card shadow-yellow-600">
+             <img src="images/logo-fire.svg" alt="">
+             <p class="text-center pt-2">
+                 Firestore
+             </p>
+        </div>
+    </article>
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,11 @@
 <script>
     import Nav  from "../components/portfolio-nav.svelte";
     import Home from "../components/portfolio-home.svelte";
+    import About from "../components/portfolio-aboutme.svelte";
+    import Skills from "../components/portfolio-skills.svelte";
 </script>
 
 <Nav />
 <Home />
+<About />
+<Skills />
